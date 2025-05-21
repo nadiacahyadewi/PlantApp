1917d9f221f5eeb79a88325d4df17d17e0fd52d6
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2025-05-21 23:52:40 +0700

    menambahkan import

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:plantapp/constants.dart';
+import 'package:plantapp/screens/home/components/Featured_plants.dart';
 import 'package:plantapp/screens/home/components/header_with_searchbox.dart';
 import 'package:plantapp/screens/home/components/recomend_plant.dart';
 
 import 'header_with_searchbox.dart';
 import 'title_with_more_button.dart';
+import 'recomend_plant.dart';
+import 'Featured_plants.dart';
 
 class Body extends StatelessWidget {
   const Body({super.key});
@@ -28,47 +31,3 @@ class Body extends StatelessWidget {
   }
 }
 
-class FeaturedPlants extends StatelessWidget {
-  const FeaturedPlants({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      scrollDirection: Axis.horizontal,
-      child: Row(
-        children: [
-          FeaturePlantCard(image: "assets/images/tanaman 1.jpg", press: () {}),
-          FeaturePlantCard(image: "assets/images/tanaman 1.jpg", press: () {}),
-        ],
-      ),
-    );
-  }
-}
-
-class FeaturePlantCard extends StatelessWidget {
-  const FeaturePlantCard({super.key, required this.image, required this.press});
-
-  final String image;
-  final VoidCallback press;
-
-  @override
-  Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    return GestureDetector(
-      onTap: press,
-      child: Container(
-        margin: EdgeInsets.only(
-          left: kDefaultPadding,
-          top: kDefaultPadding / 2,
-          bottom: kDefaultPadding / 2,
-        ),
-        width: size.width * 0.8,
-        height: 185,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          image: DecorationImage(fit: BoxFit.cover, image: AssetImage(image)),
-        ),
-      ),
-    );
-  }
-}
