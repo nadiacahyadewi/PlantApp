8bf4e9e19be67ee967f521c92c1bacd3ef0b39a3
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2025-05-21 20:51:20 +0700

    membuat text "Recomended" dan button "More"

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:plantapp/constants.dart';
+import 'package:plantapp/screens/home/components/header_with_searchbox.dart';
 
 class Body extends StatelessWidget {
   const Body({super.key});
@@ -12,61 +13,59 @@ class Body extends StatelessWidget {
       child: Column(
         children: <Widget>[
           HeaderWithSearchBox(size: size),
+          TitleWithMoreBtn( 
+            title: "Recomended",
+            press: (){},
+          ),
         ],
       ),
     );
   }
 }
 
-class HeaderWithSearchBox extends StatelessWidget {
-  const HeaderWithSearchBox({
-    super.key,
-    required this.size,
+class TitleWithMoreBtn extends StatelessWidget {
+  const TitleWithMoreBtn({
+    super.key, required this.title, required this.press,
   });
 
-  final Size size;
+  final String title;
+  final Function press;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+      child: Row(
+        children: [
+          TitleWithCustomUnderline(text: title),
+          Spacer(),
+          FilledButton(
+            onPressed: (){},
+            style: FilledButton.styleFrom(backgroundColor: kPrimaryColor),
+            child: const Text("More"),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class TitleWithCustomUnderline extends StatelessWidget {
+  const TitleWithCustomUnderline({super.key, required this.text});
+
+  final String text;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.only(bottom: kDefaultPadding * 2.5),
-      height: size.height * 0.2,
+      height: 24,
       child: Stack(
         children: [
-          Container(
-            padding: EdgeInsets.only(
-              left: kDefaultPadding,
-              right: kDefaultPadding,
-              bottom: 36 + kDefaultPadding,
-            ),
-            height: size.height * 0.2 - 27,
-            decoration: const BoxDecoration(
-              color: kPrimaryColor,
-              borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(36),
-                bottomRight: Radius.circular(36),
-              ),
-            ),
-            child: Row(
-              children: [
-                Text(
-                  'Hi Uishopy!',
-                  style: Theme.of(
-                    context,
-                  ).textTheme.headlineSmall?.copyWith(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                const Spacer(),
-                CircleAvatar(
-                  radius: 30,
-                  backgroundColor: Colors.white,
-                  backgroundImage: AssetImage(
-                    'assets/images/profile.png',
-                  ),
-                ),
-              ],
+          Padding(
+            padding: const EdgeInsets.only(left: kDefaultPadding / 4),
+            child: Text(
+              text,
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
           ),
           Positioned(
@@ -74,37 +73,9 @@ class HeaderWithSearchBox extends StatelessWidget {
             left: 0,
             right: 0,
             child: Container(
-              alignment: Alignment.center,
-              margin: EdgeInsets.symmetric(horizontal: kDefaultPadding),
-              padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
-              height: 54,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(20),
-                boxShadow: [
-                  BoxShadow(
-                    offset: Offset(0, 10),
-                    blurRadius: 50,
-                    color: kPrimaryColor.withOpacity(0.23),
-                  ),
-                ],
-              ),
-              child: TextField(
-                onChanged: (value) {},
-                decoration: InputDecoration(
-                  hintText: "Search",
-                  hintStyle: TextStyle(
-                    color: kPrimaryColor.withOpacity(0.5),
-                  ),
-                  enabledBorder: InputBorder.none,
-                  focusedBorder: InputBorder.none,
-    
-                  suffixIcon: Icon(
-                    Icons.search,
-                    color: Color.fromRGBO(12, 152, 105, 1),
-                  ),
-                ),
-              ),
+              margin: EdgeInsets.only(right: kDefaultPadding / 4),
+              height: 7,
+              color: kPrimaryColor.withOpacity(0.2),
             ),
           ),
         ],
