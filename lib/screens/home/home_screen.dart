c5bf708438bf8a810696a0ff7ed5688320acf9aa
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2025-05-21 17:41:39 +0700

    menambahkan Appbar dan icon didalamnya

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:plantapp/screens/home/components/body.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: buildAppBar(context),
+      body: const Body(),
+    );
+  }
+
+  AppBar buildAppBar(BuildContext context) {
+    return AppBar(
+      elevation: 0,
+      backgroundColor: const Color.fromRGBO(12, 152, 105, 1), // warna AppBar
+      leading: IconButton(
+        icon: const Icon(Icons.menu, color: Colors.white), // warna ikon
+        onPressed: () {},
+      ),
+    );
   }
 }
