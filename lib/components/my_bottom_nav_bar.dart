01ee037ef21582ba6e06be2e9f8767ee49df041f
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2025-05-21 23:43:40 +0700

    memindahkan kodingan navbar dari home_screen.dart ke my_bottom_nav_bar.dart

---
+++ lib/components/my_bottom_nav_bar.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:plantapp/constants.dart';
+
+class MyBottomNavBar extends StatelessWidget {
+  const MyBottomNavBar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(
+        left: kDefaultPadding * 2,
+        right: kDefaultPadding * 2,
+        bottom: kDefaultPadding, 
+        ),
+      height: 80,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            offset: Offset(0, -10),
+            blurRadius: 35,
+            color: kPrimaryColor.withOpacity(0.38),
+          ),
+        ],
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          IconButton(
+            icon: Icon(Icons.home), 
+            iconSize: 35, 
+            onPressed: () {}
+          ),
+          IconButton(
+            icon: Icon(Icons.camera), 
+            iconSize: 35, 
+            onPressed: () {}
+          ),
+          IconButton(
+            icon: Icon(Icons.pin_drop), 
+            iconSize: 35, 
+            onPressed: () {}
+          ),
+          IconButton(
+            icon: Icon(Icons.person), 
+            iconSize: 35, 
+            onPressed: () {}
+          ),
+          
+        ],
+      ),
+    );
+  }
+}
