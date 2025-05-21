14a11a89a842a0df7848e3b65d39a9a3b0a5bf51
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2025-05-21 23:40:36 +0700

    membuat navbar

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:plantapp/constants.dart';
 import 'package:plantapp/screens/home/components/body.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -8,7 +10,8 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: buildAppBar(context),
-      body: const Body(),
+      body: Body(),
+      bottomNavigationBar: MyBottomNavBar(),
     );
   }
 
@@ -19,6 +22,60 @@ class HomeScreen extends StatelessWidget {
       leading: IconButton(
         icon: const Icon(Icons.menu, color: Colors.white), // warna ikon
         onPressed: () {},
+      ),
+    );
+  }
+}
+
+class MyBottomNavBar extends StatelessWidget {
+  const MyBottomNavBar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(
+        left: kDefaultPadding * 2,
+        right: kDefaultPadding * 2,
+        bottom: kDefaultPadding, 
+        ),
+      height: 80,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            offset: Offset(0, -10),
+            blurRadius: 35,
+            color: kPrimaryColor.withOpacity(0.38),
+          ),
+        ],
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          IconButton(
+            icon: Icon(Icons.home), 
+            iconSize: 35, 
+            onPressed: () {}
+          ),
+          IconButton(
+            icon: Icon(Icons.camera), 
+            iconSize: 35, 
+            onPressed: () {}
+          ),
+          IconButton(
+            icon: Icon(Icons.pin_drop), 
+            iconSize: 35, 
+            onPressed: () {}
+          ),
+          IconButton(
+            icon: Icon(Icons.person), 
+            iconSize: 35, 
+            onPressed: () {}
+          ),
+          
+        ],
       ),
     );
   }
