1c255d70ed88b699866c96a5518064acd910d36a
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2025-05-21 18:29:00 +0700

    menambahkan search bar

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -23,6 +23,42 @@ class Body extends StatelessWidget {
                   ),
                 ),
               ),
+              Positioned(
+                bottom: 0,
+                left: 0,
+                right: 0,
+                child: Container(
+                  alignment: Alignment.center ,
+                  margin: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                  padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                  height: 54,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: [
+                      BoxShadow(
+                        offset: Offset(0, 10),
+                        blurRadius: 50,
+                        color: kPrimaryColor.withOpacity(0.23),
+                      ),
+                    ],
+                  ),
+                  child: TextField(
+                    onChanged: (value) {} ,
+                    decoration: InputDecoration(
+                      hintText: "Search",
+                      hintStyle: TextStyle(
+                        color: kPrimaryColor.withOpacity(0.5),
+                      ),
+                      enabledBorder: InputBorder.none,
+                      focusedBorder: InputBorder.none,
+
+                      suffixIcon: Icon(Icons.search, color: Color.fromRGBO(12, 152, 105, 1)
+                      ),
+                    ),
+                  ),
+                ),
+              ),
             ],
           ),
         ),
