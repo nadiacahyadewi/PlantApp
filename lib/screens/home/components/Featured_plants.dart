fa87546dbae5f1be2d15b545ef105e3939fd516e
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2025-05-21 23:07:39 +0700

    Membuat FeaturedPlants

--- lib/screens/home/components/Featured_plants.dart
+++ lib/screens/home/components/Featured_plants.dart
@@ -1,32 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:plantapp/constants.dart';
-import 'package:plantapp/screens/home/components/header_with_searchbox.dart';
-import 'package:plantapp/screens/home/components/recomend_plant.dart';
-
-import 'header_with_searchbox.dart';
-import 'title_with_more_button.dart';
-
-class Body extends StatelessWidget {
-  const Body({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-
-    return SingleChildScrollView(
-      child: Column(
-        children: <Widget>[
-          HeaderWithSearchBox(size: size),
-          TitleWithMoreBtn(title: "Recomended", press: () {}),
-          RecomendsPlant(),
-          TitleWithMoreBtn(title: "Featured Plant", press: () {}),
-          FeaturedPlants(),
-          SizedBox(height: kDefaultPadding),
-        ],
-      ),
-    );
-  }
-}
 
 class FeaturedPlants extends StatelessWidget {
   const FeaturedPlants({super.key});
