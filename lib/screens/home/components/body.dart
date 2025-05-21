e89ce0f5f82eaf9de994d720b0cb9b92967baf22
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2025-05-21 17:47:28 +0700

    membuat container yang setiap sudutnya melengkung

---
+++ lib/screens/home/components/body.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:plantapp/constants.dart';
+
+class Body extends StatelessWidget {
+  const Body({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Column(
+      children: <Widget>[
+        Container(
+          height: size.height * 0.2,
+          child: Stack(
+            children: [
+              Container(
+                height: size.height * 0.2 - 27,
+                decoration: const BoxDecoration(
+                  color: kPrimaryColor,
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(36),
+                    bottomRight: Radius.circular(36),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
