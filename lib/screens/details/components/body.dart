0b7984c6549d50f3d8fcdbef6627e9739699ebc1
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2025-05-22 21:34:40 +0700

    membuat class ImageAndIcons di body.dart

--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -11,64 +11,81 @@ class Body extends StatelessWidget {
     return SingleChildScrollView(
       child: Column(
         children: [
-          SizedBox(
-            height: size.height * 0.8,
-            child: Row(
-              children: [
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                      vertical: kDefaultPadding * 3,
-                    ),
-                    child: Column(
-                      children: [
-                        Align(
-                          alignment: Alignment.topLeft,
-                          child: IconButton(
-                            padding: EdgeInsets.symmetric(
-                              horizontal: kDefaultPadding,
-                            ),
-                            icon: Icon(Icons.arrow_back_outlined),
-                            onPressed: () {
-                              Navigator.pop(context);
-                            },
-                          ),
-                        ),
-                        const Spacer(),
-                        IconCard(icon: Icons.wb_sunny_outlined, size: 50.0, color: kPrimaryColor),
-                        IconCard(icon: Icons.thermostat, size: 50.0, color: kPrimaryColor),
-                        IconCard(icon: Icons.water_drop_outlined, size: 50.0, color: kPrimaryColor),
-                        IconCard(icon: Icons.air, size: 50.0, color: kPrimaryColor),
-                      ],
-                    ),
-                  ),
-                ),
-                Container(
-                  height: size.height * 0.8,
-                  width: size.width * 0.75,
-                  decoration: BoxDecoration(
-                    borderRadius: const BorderRadius.only(
-                      topLeft: Radius.circular(63),
-                      bottomLeft: Radius.circular(63),
-                    ),
-                    boxShadow: [
-                      BoxShadow(
-                        offset: const Offset(0, 10),
-                        blurRadius: 60,
-                        color: kPrimaryColor.withOpacity(0.29),
-                      ),
-                    ],
-                    image: const DecorationImage(
-                      alignment: Alignment.centerLeft,
-                      fit: BoxFit.cover,
-                      image: AssetImage("assets/images/tanaman 1.jpg"),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
+          ImageAndIcons(size: size),
         ],
+      ),
+    );
+  }
+}
+
+class ImageAndIcons extends StatelessWidget {
+  const ImageAndIcons({
+    super.key,
+    required this.size,
+  });
+
+  final Size size;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: kDefaultPadding * 3),
+      child: SizedBox(
+        height: size.height * 0.8,
+        child: Row(
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  vertical: kDefaultPadding * 3,
+                ),
+                child: Column(
+                  children: [
+                    Align(
+                      alignment: Alignment.topLeft,
+                      child: IconButton(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: kDefaultPadding,
+                        ),
+                        icon: Icon(Icons.arrow_back_outlined),
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                      ),
+                    ),
+                    const Spacer(),
+                    IconCard(icon: Icons.wb_sunny_outlined, size: 50.0, color: kPrimaryColor),
+                    IconCard(icon: Icons.thermostat, size: 50.0, color: kPrimaryColor),
+                    IconCard(icon: Icons.water_drop_outlined, size: 50.0, color: kPrimaryColor),
+                    IconCard(icon: Icons.air, size: 50.0, color: kPrimaryColor),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              height: size.height * 0.8,
+              width: size.width * 0.75,
+              decoration: BoxDecoration(
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(63),
+                  bottomLeft: Radius.circular(63),
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    offset: const Offset(0, 10),
+                    blurRadius: 60,
+                    color: kPrimaryColor.withOpacity(0.29),
+                  ),
+                ],
+                image: const DecorationImage(
+                  alignment: Alignment.centerLeft,
+                  fit: BoxFit.cover,
+                  image: AssetImage("assets/images/tanaman 1.jpg"),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
