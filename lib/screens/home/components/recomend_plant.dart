04a752ffa80becdc111206cccbdb7ae9bfa3b461
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2025-05-22 23:45:00 +0700

    menambahkan navigator ke detailscreen

--- lib/screens/home/components/recomend_plant.dart
+++ lib/screens/home/components/recomend_plant.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:plantapp/constants.dart';
+import 'package:plantapp/screens/details/details_screen.dart';
 
 class RecomendsPlant extends StatelessWidget {
-  const RecomendsPlant({
-    super.key,
-  });
+  const RecomendsPlant({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -17,28 +16,56 @@ class RecomendsPlant extends StatelessWidget {
             title: "Samantha",
             country: "Russia",
             price: 440,
-            press: () {},
+            press: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => DetailsScreen(),
+                ),
+              );
+            },
           ),
           RecomendPlantCard(
             image: "assets/images/tanaman 1.jpg",
             title: "Samantha",
             country: "Russia",
             price: 440,
-            press: () {},
+            press: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => DetailsScreen(),
+                ),
+              );
+            },
           ),
           RecomendPlantCard(
             image: "assets/images/tanaman 1.jpg",
             title: "Samantha",
             country: "Russia",
             price: 440,
-            press: () {},
+            press: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => DetailsScreen(),
+                ),
+              );
+            },
           ),
           RecomendPlantCard(
             image: "assets/images/tanaman 1.jpg",
             title: "Samantha",
             country: "Russia",
             price: 440,
-            press: () {},
+            press: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => DetailsScreen(),
+                ),
+              );
+            },
           ),
         ],
       ),
@@ -48,7 +75,12 @@ class RecomendsPlant extends StatelessWidget {
 
 class RecomendPlantCard extends StatelessWidget {
   const RecomendPlantCard({
-    super.key, required this.image, required this.title, required this.country, required this.price, required this.press,
+    super.key,
+    required this.image,
+    required this.title,
+    required this.country,
+    required this.price,
+    required this.press,
   });
 
   final String image, title, country;
@@ -77,7 +109,7 @@ class RecomendPlantCard extends StatelessWidget {
                 borderRadius: BorderRadius.only(
                   bottomLeft: Radius.circular(10),
                   bottomRight: Radius.circular(10),
-                  ),
+                ),
                 boxShadow: [
                   BoxShadow(
                     offset: Offset(0, 10),
@@ -93,8 +125,9 @@ class RecomendPlantCard extends StatelessWidget {
                       children: [
                         TextSpan(
                           text: "$title\n".toUpperCase(),
-                          style: Theme.of(context).textTheme.labelLarge
-                              ?.copyWith(color: Colors.black),
+                          style: Theme.of(
+                            context,
+                          ).textTheme.labelLarge?.copyWith(color: Colors.black),
                         ),
                         TextSpan(
                           text: country.toUpperCase(),
