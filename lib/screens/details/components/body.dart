dbd6066f2f4878d665e5a0c19629b29df6995675
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2025-05-22 23:48:07 +0700

    membuat button di detail

--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:plantapp/constants.dart';
 import 'package:plantapp/screens/details/components/icon_card.dart';
 import 'package:plantapp/screens/details/components/image_and_icons.dart';
+import 'package:plantapp/screens/details/components/title_and_price.dart';
 
 class Body extends StatelessWidget {
   const Body({super.key});
@@ -13,60 +14,44 @@ class Body extends StatelessWidget {
       child: Column(
         children: [
           ImageAndIcons(size: size),
-          TitleAndPrice(
-            title: "Angelica",
-            country: "Russia",
-            price: 440,
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class TitleAndPrice extends StatelessWidget {
-  const TitleAndPrice({
-    super.key, required this.title, required this.country, required this.price,
-  });
-  
-  final String title, country;
-  final int price;
-
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
-      child: Row(
-        children: [
-          RichText(
-            text: TextSpan(
-              children: [
-                TextSpan(
-                  text: "$title\n",
-                  style: Theme.of(
-                    context,
-                  ).textTheme.headlineMedium?.copyWith(
-                    color: kTextColor,
-                    fontWeight: FontWeight.bold,
+          TitleAndPrice(title: "Angelica", country: "Russia", price: 440),
+          SizedBox(height: kDefaultPadding),
+          Row(
+            children: [
+              SizedBox(
+                width: size.width / 2,
+                height: 84,
+                child: FilledButton(
+                  style: FilledButton.styleFrom(
+                    backgroundColor: kPrimaryColor,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(20),
+                      ), 
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: Text(
+                    "Buy Now",
+                    style: TextStyle(color: Colors.white, fontSize: 16),
                   ),
                 ),
-                TextSpan(
-                  text: country,
-                  style: TextStyle(
-                    fontSize: 20,
-                    color: kPrimaryColor,
-                    fontWeight: FontWeight.w300,
+              ),
+              Expanded(
+                child: FilledButton(
+                  onPressed: () {},
+                  style: ButtonStyle(
+                    backgroundColor: WidgetStateProperty.all<Color>(
+                      Colors.transparent,
+                    ),
+                  ),
+                  child: Text("Description", style: TextStyle(color: Colors.black),
                   ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
-          Spacer(),
-          Text("\$$price", style: Theme.of(context)
-          .textTheme
-          .headlineSmall?.copyWith(color: kPrimaryColor),
-          ),
+          //SizedBox(height: kDefaultPadding * 2),
         ],
       ),
     );
