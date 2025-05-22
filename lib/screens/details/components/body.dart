0f8733b2aec12b37f2784abb4ca9dd775effff91
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2025-05-22 21:28:51 +0700

    membuat icon card di body.dart

---
+++ lib/screens/details/components/body.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:plantapp/constants.dart';
+import 'package:plantapp/screens/details/components/icon_card.dart';
+
+class Body extends StatelessWidget {
+  const Body({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          SizedBox(
+            height: size.height * 0.8,
+            child: Row(
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: kDefaultPadding * 3,
+                    ),
+                    child: Column(
+                      children: [
+                        Align(
+                          alignment: Alignment.topLeft,
+                          child: IconButton(
+                            padding: EdgeInsets.symmetric(
+                              horizontal: kDefaultPadding,
+                            ),
+                            icon: Icon(Icons.arrow_back_outlined),
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                          ),
+                        ),
+                        const Spacer(),
+                        IconCard(icon: Icons.wb_sunny_outlined, size: 50.0, color: kPrimaryColor),
+                        IconCard(icon: Icons.thermostat, size: 50.0, color: kPrimaryColor),
+                        IconCard(icon: Icons.water_drop_outlined, size: 50.0, color: kPrimaryColor),
+                        IconCard(icon: Icons.air, size: 50.0, color: kPrimaryColor),
+                      ],
+                    ),
+                  ),
+                ),
+                Container(
+                  height: size.height * 0.8,
+                  width: size.width * 0.75,
+                  decoration: BoxDecoration(
+                    borderRadius: const BorderRadius.only(
+                      topLeft: Radius.circular(63),
+                      bottomLeft: Radius.circular(63),
+                    ),
+                    boxShadow: [
+                      BoxShadow(
+                        offset: const Offset(0, 10),
+                        blurRadius: 60,
+                        color: kPrimaryColor.withOpacity(0.29),
+                      ),
+                    ],
+                    image: const DecorationImage(
+                      alignment: Alignment.centerLeft,
+                      fit: BoxFit.cover,
+                      image: AssetImage("assets/images/tanaman 1.jpg"),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
